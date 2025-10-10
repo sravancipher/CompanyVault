bf6f3b0fcd13e51bd4095a9baef95c85bf59f35c
sra1potnuru <sravan.potnuru@parabola9.com>
2025-10-10 16:15:43 +0530

    changes made for the deployment

---
+++ server.js
@@ -0,0 +1,19 @@
+import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(3001, () => {
+  console.log("JSON Server is running");
+});
+
+export default server;

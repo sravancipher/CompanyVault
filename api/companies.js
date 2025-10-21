6c9470dea573d8b2b625d11e6f0d8c7d5df3fc6a
sra1potnuru <sravan.potnuru@parabola9.com>
2025-10-21 07:58:14 +0530

    web with version 2 changes

--- api/companies.js
+++ api/companies.js
@@ -1,31 +1,10 @@
-// import fs from "fs";
-// import path from "path";
+import fs from "fs";
+import path from "path";
 
-// export default function handler(req, res) {
+export default function handler(req, res) {
 
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-  
-//   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-//   if (req.method === "OPTIONS") {
-//     res.status(200).end();
-//     return;
-//   }
-
-//   const filePath = path.join(process.cwd(), "db", "db.json");
-//   const fileContents = fs.readFileSync(filePath, "utf-8");
-//   const data = JSON.parse(fileContents);
-
-//   res.status(200).json(data.companies); 
-// }
-
-
-const fs = require("fs");
-const path = require("path");
-
-module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
@@ -38,5 +17,6 @@ module.exports = (req, res) => {
   const fileContents = fs.readFileSync(filePath, "utf-8");
   const data = JSON.parse(fileContents);
 
-  res.status(200).json(data.companies);
-};
+  res.status(200).json(data.companies); 
+}
+
